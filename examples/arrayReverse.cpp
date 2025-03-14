ee0404736a9bf0fab1a171f1fbf413acc58916c3
abhigargrepo <abhigarg@amd.com>
2025-03-14 08:14:14 +0000

    Adding arrayReverse example

---
+++ examples/arrayReverse.cpp
@@ -0,0 +1,59 @@
+#include <hip/hip_runtime.h>
+#include <iostream>
+
+#define N 20 // Vector size
+
+__global__ void reverse_array(float *a) {
+  __shared__ float sData[N];
+
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  sData[threadIdx.x] = a[idx];
+
+  __syncthreads();
+
+  a[idx] = sData[blockDim.x - 1 - threadIdx.x];
+}
+
+int main() {
+  // Host memory allocation
+  float *h_A, *h_B;
+  h_A = new float[N];
+  h_B = new float[N];
+
+  // Initialize host data
+  for (int i = 0; i < N; i++)
+    h_A[i] = i * 1.0f;
+
+  // Device memory allocation
+  float *d_A;
+  hipMalloc((void **)&d_A, N * sizeof(float));
+
+  // Copy data from Host to Device
+  hipMemcpy(d_A, h_A, N * sizeof(float), hipMemcpyHostToDevice);
+
+  // Launch HIP Kernel
+  reverse_array<<<1, N>>>(d_A);
+
+  // Copy result from Device to Host
+  hipMemcpy(h_B, d_A, N * sizeof(float), hipMemcpyDeviceToHost);
+
+  // Print result
+  std::cout << "Original Array:" << std::endl;
+  for(int i=0; i<N; i++)
+    std::cout << h_A[i] << " ";
+  std::cout << std::endl;
+  std::cout << "Reverse Array:" << std::endl;
+
+  for(int i=0; i<N; i++)
+    std::cout << h_B[i] << " ";
+  std::cout << std::endl;
+  
+  // Free device memory
+  hipFree(d_A);
+
+  // Free host memory
+  delete[] h_A;
+  delete[] h_B;
+
+  return 0;
+}
